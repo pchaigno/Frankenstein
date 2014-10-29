854b35bbdb511645fe4d4bfd2c36d4bd6ecf1465
Paul Chaignon <paul.chaignon@gmail.com>
2014-10-29 11:57:27 -0400

    Make a module out of gauss.py

--- gauss.py
+++ gauss.py
@@ -10,13 +10,18 @@ def gauss():
 	return numbers
 
 
-if __name__ == "__main__":
+def gauss_range(min_sum, max_sum):
 	numbers = gauss()
 	total = sum(numbers)
 	while total < 50 or total > 60:
 		numbers = gauss()
 		total = sum(numbers)
+	return numbers
 
+
+if __name__ == "__main__":
+	numbers = gauss_range(50, 60)
+	total = sum(numbers)
 	mean = total / len(numbers)
 	print("sum=%d" % total)
 	print("mean=%f" % mean)
