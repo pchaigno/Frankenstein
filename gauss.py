45031ec32f33dbb545516fa827899fe5e44c66ce
Paul Chaignon <paul.chaignon@gmail.com>
2014-10-29 12:57:57 -0400

    Redistribute commit dates to squash them in a month

--- gauss.py
+++ gauss.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import random
 
-def gauss():
+def generate():
 	numbers = []
 	for i in range(0, 29):
 		number = (int)(random.gauss(5/3, 1))
@@ -10,18 +10,19 @@ def gauss():
 	return numbers
 
 
-def gauss_range(min_sum, max_sum):
-	numbers = gauss()
+def generate_in_range(min_sum, max_sum):
+	numbers = generate()
 	total = sum(numbers)
 	while total < 50 or total > 60:
-		numbers = gauss()
+		numbers = generate()
 		total = sum(numbers)
 	return numbers
 
 
 if __name__ == "__main__":
-	numbers = gauss_range(50, 60)
+	numbers = generate_in_range(50, 60)
 	total = sum(numbers)
 	mean = total / len(numbers)
+	print("len=%d" % len(numbers))
 	print("sum=%d" % total)
 	print("mean=%f" % mean)
